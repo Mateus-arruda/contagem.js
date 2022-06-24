514d7cd88230afe7825c1cef074b3da820d1a35b
Mateus Arruda <arrudamateus02@gmail.com>
2022-06-24 18:53:13 -0400

    contagem.js

---
+++ contagem.js
@@ -0,0 +1,13 @@
+let listaAlunos = ["Victor", "Joao", "Carlos", "Fernando", "Mateus", "Nicolas"]
+
+for(let numero = 0; numero < listaAlunos.length; numero++){
+    if(numero == 0) {
+        console.log("numero eh zero")
+     } else if(numero % 2 == 0) {
+        console.log(`o numero ${numero} eh par`)
+     } else if(numero % 2 == 1) {
+console.log(`o numero ${numero} eh impar`) 
+     }
+}    
+    
+ 
